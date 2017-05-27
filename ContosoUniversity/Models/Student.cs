6e013cae1eafea9880d000f1db416f26a903e917
Rogelio Quintana Romero <oilegor_rqr@hotmail.com>
2017-05-27 01:15:45 -0500

    5 of 10 Creating a complex data model

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        //[Required]        requiere lo reemplazamos con un minumo de caractes
+        // que debe de ingresar el usuario
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Display(Name ="Last Name")]
+        [StringLength(50, MinimumLength = 1)]
         public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name ="First Name")]
         public string FirstMidName { get; set; }
+
+        [RegularExpression(@"^[a-z''-'@.\s]*$")]
         public string EmailAddress { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
 
